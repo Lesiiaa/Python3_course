ed84220d60fa98d384a1e8d9cbff584a5d0a71b6
Letycja Niemiec <lesia@MacBook-Air-Letycja.local>
2024-07-01 22:34:54 +0200

    Section 3 - libraries

---
+++ Section_3_Libraries_and_functions/importing_libraries.py
@@ -0,0 +1,12 @@
+###importing libraries - trying them out###
+print(abs(-2))
+
+import math
+print(math.sqrt(4))
+
+name = "feLIcia"
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+
+
---
+++ Section_3_Libraries_and_functions/importing_libraries_tasks.py
@@ -0,0 +1,18 @@
+###tasks from lesson about impoting libraries###
+#Task 1
+#Increase the first letter of the given variable: name = "adam"
+name = "adam"
+name = name.capitalize()
+print(name)
+
+#Task 2
+#Change letters to lowercase letters in the given variable: name = "GenOWefa"
+name = "GenOWefa"
+name = name.lower()
+print(name)
+
+#Task 3
+#Change letters to uppercase letters in the given variable: name = "Anastazja"
+name = "Anastazja"
+name = name.upper()
+print(name)
