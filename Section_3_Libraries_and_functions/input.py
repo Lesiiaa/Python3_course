030ec5a8097985ab76a8600ab9cc57b720469cce
Letycja Niemiec <lesia@MacBook-Air-Letycja.local>
2024-07-02 23:15:00 +0200

    input lesson

---
+++ Section_3_Libraries_and_functions/input.py
@@ -0,0 +1,5 @@
+###collecting data from user - input###
+print("Calculator that adds two numbers")
+a = int(input("Give the first number: "))
+b = int(input("Give the second number: "))
+print("The result of the addition:", a+b)
---
+++ Section_3_Libraries_and_functions/input_tasks.py
@@ -0,0 +1,13 @@
+###tasks from lesson about collecting data from user###
+#Task 1
+#Create a program that asks the user to enter their name, age, and favorite color.
+#Write down values in an eye-pleasing way. Write down how old the user is and how old they will be in a year.
+print("Hi! Give me some information so I know more about u!")
+name = input("What's your name? -> ")
+age = int(input("How old are u? -> "))
+colour = input("What is your favorite colour? -> ")
+
+print("\n")
+print(f"Hi {name}! Your favorite colour is {colour}.\n\
+You are {age} years old.\n\
+You will be {age + 1} next year.")
