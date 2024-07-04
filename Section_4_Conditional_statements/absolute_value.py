656da9b2d3afffce5986234ecc30e744e29ee836
Letycja <letycja.niemiec@gmail.com>
2024-07-04 12:34:46 +0200

    text change

--- Section_4_Conditional_statements/absolute_value.py
+++ Section_4_Conditional_statements/absolute_value.py
@@ -1,8 +1,8 @@
 ###absolute value###
-print("Program to return absolute number")
+print("Program to return absolute value")
 number = int(input("Number: "))
 
 if(number < 0):
     number = -number
 
-print(f"Absolute number: {number}")
+print(f"Absolute value: {number}")
