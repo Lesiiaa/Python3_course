7e83305c4b003967d324e09c5771c6bb4189783a
Letycja <letycja.niemiec@gmail.com>
2024-07-16 17:59:40 +0200

    nested data

---
+++ Section_7_Advanced_data_types/nested_data.py
@@ -0,0 +1,32 @@
+###creating nested data###
+guest_list = [
+["Julie", 23, "female"],
+["Johnny", 25, "male"],
+["Georgia", 17, "female"]
+]
+
+guest_list_2 = {
+("Olivia", 30, "female"),
+("Rick", 70, "male"),
+("Morty", 14, "male")
+}
+
+guest_list_3 = {
+("Rick", 70, "male"),
+("Zack", 70, "male"),
+("Zeref", 14, "male")
+}
+
+print(guest_list[0])
+print(guest_list[1][1])
+
+guest_list[0][1] = 29
+guest_list.append(["Fabio", 40, "male"])
+print(guest_list)
+
+
+guest_list_4 = guest_list_2 & guest_list_3
+print(guest_list_4)
+
+
+
